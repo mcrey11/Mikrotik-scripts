ff35cb86df41d566dae118c047e2dad7f2a8256e
BlackVS <vvs@coders.in.ua>
2020-03-25 11:41:31 +0200

    OpenVPN scripts added

---
+++ modules/securedROS/src/secureROS/settings.py
@@ -0,0 +1,14 @@
+#################################################################################
+# Set only in this values optionss
+# please make this file accessible only for root! 
+
+app_name = "secureros"  # keyword, don't change if not sure
+app_tmp_dir = "/tmp/" + app_name     #script must have write permissions to this folder
+app_log_dir = "/var/log/" + app_name #script must have write permissions to this folder
+
+
+#################################################################################
+# Options which can be modified via commandline
+#debug = False
+debug = True #unsafe!
+
